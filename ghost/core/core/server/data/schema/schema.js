8a28fe969ee0063b3013b7818effdf4533bc4508
franky19 <frankimarbun@gmail.com>
2026-03-20 14:57:46 +0700

    feat: update schema to use cascadeDelete and setNullDelete for foreign key constraints

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1178,7 +1178,7 @@ module.exports = {
             maxlength: 24,
             nullable: false,
             references: 'posts.id',
-            onDelete: 'CASCADE'
+            cascadeDelete: true
         },
 
         user_id: {
@@ -1186,11 +1186,11 @@ module.exports = {
             maxlength: 24,
             nullable: true,
             references: 'users.id',
-            onDelete: 'SET NULL'
+            setNullDelete: true
         },
 
         locked_at: {
-            type: 'timestamp',
+            type: 'dateTime',
             nullable: false
         }
     }
