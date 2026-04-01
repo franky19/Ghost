07c24f92419e9d04bbebce201cb93bf5a9207a40
Steve Larson <9larsons@gmail.com>
2026-03-31 20:29:52 -0500

    Moved publishing scheduling tests to e2e (#27031)
    ref https://linear.app/ghost/issue/PLA-10/

--- ghost/core/core/boot.js
+++ ghost/core/core/boot.js
@@ -340,7 +340,9 @@ async function initServices() {
     const statsService = require('./server/services/stats');
     const explorePingService = require('./server/services/explore-ping');
 
+    const config = require('./shared/config');
     const urlUtils = require('./shared/url-utils');
+    const schedulingApiUrl = config.get('scheduling').schedulerUrl || urlUtils.urlFor('api', {type: 'admin'}, true);
 
     // NOTE: Members service depends on these
     //       so they are initialized before it.
@@ -367,7 +369,7 @@ async function initServices() {
         emailAnalytics.init(),
         webhooks.listen(),
         scheduling.init({
-            apiUrl: urlUtils.urlFor('api', {type: 'admin'}, true)
+            apiUrl: schedulingApiUrl
         }),
         comments.init(),
         linkTracking.init(),
